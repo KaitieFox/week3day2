adf15aacdd12a575ed8b0e55f2873727c838b9df
KaitieFox <kaitiefox@gmail.com>
2017-10-10 13:05:09 -0400

    Initial Commit

---
+++ week3day2/Boat.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3day2
+{
+    class Boat
+    {
+        //fields. values we can use in program. describes characteristics. actual information about that class.
+        public int numberOfPassengers;
+
+        private string name;
+        private double engineSize; 
+
+        //properties. this is so we can expose this private field to other parts of our program.
+        public string Name
+        {
+            get { return name; }   //could be ambiguity. to use 'this.' confines it to the class.
+            set { name = value; }
+        }
+
+        //constructor
+        public Boat()
+        {
+
+        }
+
+        public Boat(int numberOfPassengers, string name, double engineSize)
+        {
+            this.numberOfPassengers = numberOfPassengers;
+            this.name = name;
+            this.engineSize = engineSize;
+        }
+
+        //methods
+        public void Move()
+        {
+            //code to make boat move
+            Console.WriteLine(name + " moves forward.");
+
+
+
+        }
+
+        private double CalculateMPG() //private so we don't expose the details to it. So how do we get it? Look down.
+        {
+            //code to calculate MPG
+            double MPG = engineSize * .7d;
+
+            return MPG;
+        }
+
+        public void BoatInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Passengers: " + numberOfPassengers);
+            Console.WriteLine("Engine Size: " + engineSize);
+            Console.WriteLine("MPG: " + CalculateMPG()); //you can call a method in a method. 
+            Console.WriteLine();
+        }
+
+
+
+
+    }
+}
---
+++ week3day2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boat boatyMcBoatface = new Boat(3, "Boaty McBoatface", 18.6); //create the new boat instance. for another needs a new variable.
+            boatyMcBoatface.Move();
+            boatyMcBoatface.BoatInfo();
+
+            Boat boatSirAttenborough = new Boat(14, "RRS Sir David Attenborough", 30.8);
+            boatSirAttenborough.BoatInfo();
+
+
+              
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
